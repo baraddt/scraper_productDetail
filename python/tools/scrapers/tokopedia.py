eed7eeadf0f807ee46f83268d5cbfde83da73e50
Mubarad <barad.fhx@gmail.com>
2025-06-02 16:04:43 +0700

    tteeaii

--- python/tools/scrapers/tokopedia.py
+++ python/tools/scrapers/tokopedia.py
@@ -5,9 +5,12 @@ import json
 def scrape(url: str):
     with sync_playwright() as p:
         browser = p.chromium.launch(headless=True)
-        context = browser.new_context(ignore_https_errors=True)
+        context = browser.new_context(
+            user_agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+            ignore_https_errors=True,
+        )
         page = context.new_page()
-        page.goto(url)
+        page.goto(url, wait_until="domcontentloaded")
         page.wait_for_timeout(3000)
 
         try:
